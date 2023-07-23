6da9c6e57ae6691f92ceb396a5cba7980fab9b85
Andrew <leducanh017@gmail.com>
2023-07-23 14:06:42 +0300

    Rename all directory to good name, solve problem C L11

--- Lecture11_FloydWarshall/C_Arbitrage.py
+++ Lecture11_FloydWarshall/C_Arbitrage.py
@@ -22,3 +22,53 @@ FrenchFranc 0.19 USDollar
 
 0
 '''
+def FloydWarshall(dist, n):
+    for k in range(n):
+        for i in range(n):
+            if dist[i][k] == INF:
+                continue
+            for j in range(n):
+                if dist[k][j] != INF and dist[i][j] < dist[i][k] * dist[k][j]:
+                    dist[i][j] = dist[i][k] * dist[k][j]
+    for i in range(n):
+        if dist[i][i] > 1.0:
+            return True
+    return False
+
+
+INF = -10**9
+
+n = -1
+tc = 1
+while n != 0:
+    n = int(input())
+    if n != 0:
+        graph = [[INF for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    graph[i][j] = 1.0
+
+
+        nodes = []
+        node_to_idx = {}
+        for i in range(n):
+            currency = input()
+            nodes.append(currency)
+            node_to_idx.update({currency: i})
+
+        m = int(input())
+        for _ in range(m):
+            curr1, rate, curr2 = input().split()
+            u = node_to_idx[curr1]
+            v = node_to_idx[curr2]
+            r = float(rate)
+            graph[u][v] = r
+        input()
+
+        ans = FloydWarshall(graph, n)
+        if ans:
+            print(f'Case {tc}: Yes')
+        else:
+            print(f'Case {tc}: No')
+        tc += 1
